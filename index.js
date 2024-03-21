b334e9ccf4b760e682b8170452e5ec7f37ed61d2
sohidul-mediusware <sohidul.mediusware@gmail.com>
2024-03-21 14:56:21 +0600

    test

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+// The express.json() function is a built-in middleware function in Express. It parses incoming requests with JSON payloads and is based on body-parser.
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const port = 8000;
+
+app.get("/sum",(req,res)=>{
+    
+    const num1 = Number(req?.query?.num1);
+    const num2 = Number(req?.query?.num2);
+    
+    res.status(200).json({ input: {num1,num2} ,output:Number(num1)+Number(num2)||0 });
+})
+
+app.get("/",(req,res)=>{
+    res.status(200).json({ msg: "Welcome to test" });
+})
+
+
+
+app.listen(port, () => console.log("server started on", port));
